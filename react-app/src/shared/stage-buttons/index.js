fa7bffc1a0745ff2f564a1282396192f92691962
TonyMartin <hello@tonymartin-web.com>
2020-02-15 21:22:47 +0100

    Syllables game working

--- react-app/src/shared/stage-buttons/index.js
+++ react-app/src/shared/stage-buttons/index.js
@@ -1,27 +1,9 @@
 import React from 'react';
 import { shuffle } from '../../common/utils'
-import $ from 'jquery/dist/jquery';
-import { addPoints } from '../../common/actions'
-import { useDispatch } from "react-redux";
-
 function StageButtons(props){
 
-	const dispatch = useDispatch();
-
-	const points = (_amount)=>{
-		return dispatch(addPoints(_amount));
-	}
-
-	const selectOption = (_option, _setStageState, _addError, _currentGame)=>{
-		const isCorrect = _currentGame.name === _option;
-		_setStageState(isCorrect);
-		if(isCorrect){
-			points(5)
-			$('#success-modal').modal()
-		} else {
-			points(-1)
-			_addError();
-		}
+	const selectOption = (_option)=>{
+		props.onOptionClick(_option);
 	}
 	
 	console.log('StageButtons', {props})
@@ -37,7 +19,7 @@ function StageButtons(props){
 			<button 
 				key={i} 
 				className="btn btn-outline-primary" 
-				onClick={()=>selectOption(option, props.setStageState, props.addError, props.currentGame)}
+				onClick={()=>selectOption(option)}
 			>
 				{option}
 			</button>
